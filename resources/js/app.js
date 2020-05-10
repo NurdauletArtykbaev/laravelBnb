10f1a5f53066a8a4242fc536c4de88e68d3297fe
unknown <artykbaev.nurdaulet@mail.ru>
2020-05-10 20:00:07 +0600

    section 8: 17 FatalError component

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,10 +11,14 @@ import router from './routes'
 import VueRouter from 'vue-router';
 import moment from 'moment';
 import starRating from './shared/components/StarRaiting.vue'
+import FatalError from './shared/components/FatalError.vue'
+
 
 Vue.component('index', require('./index.vue').default);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('star-rating', starRating);
+Vue.component('fatal-error', FatalError);
+
 
 Vue.use(VueRouter)
 Vue.filter("fromNow", value => moment(value).fromNow())
---
+++ resources/js/shared/components/FatalError.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="text-center error">
+        <i class="fas fa-bomb"></i>
+        <h1 class="mt-4">Weve encountered a problem</h1>
+        <h3>Unfortunately we cannot processed</h3>
+    </div>
+</template>
+
+<style scoped>
+.error{
+    color: #999;
+    font-size: 10rem;
+}
+</style>
