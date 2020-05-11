aac85935e9edbf86f31c4f24d522fb4104172433
unknown <artykbaev.nurdaulet@mail.ru>
2020-05-11 22:34:34 +0600

    Section 10: 3 Bookable price (controller, migration, factory), Carbon date library

--- routes/api.php
+++ routes/api.php
@@ -38,4 +38,5 @@ Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityControl
        ->name('bookables.availabity.show');
 Route::get('bookables/{bookable}/reviews', 'Api\BookableReviewController')->name('bookables.reviews.index');
 Route::apiResource('reviews', 'Api\ReviewController')->only(['show', 'store']);
+Route::get('bookables/{bookable}/price', 'Api\BookablePriceController')->name('bookables.price.show');
 Route::get('/booking-by-review/{reviewKey}', 'Api\BookingByReviewController')->name('booking.by-review.show');
