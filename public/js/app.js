93e899049cc5abc4ae8604a7e29423e0cd6661ab
unknown <artykbaev.nurdaulet@mail.ru>
2020-05-17 23:42:25 +0600

    section 11: 9. Checkout form markup

--- public/js/app.js
+++ public/js/app.js
@@ -1961,9 +1961,95 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-  computed: _objectSpread(_objectSpread({}, Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapGetters"])(['itemsInBasket'])), Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapState"])({
+  computed: _objectSpread(_objectSpread({}, Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapGetters"])(["itemsInBasket"])), Object(vuex__WEBPACK_IMPORTED_MODULE_0__["mapState"])({
     basket: function basket(state) {
       return state.basket.items;
     }
@@ -7636,7 +7722,7 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 
 // module
-exports.push([module.i, "\nh6.badge[data-v-c7cce5be]{\r\n    font-size: 100%;\n}\na[data-v-c7cce5be]{\r\n    color: black;\n}\r\n", ""]);
+exports.push([module.i, "\nh6.badge[data-v-c7cce5be] {\r\n    font-size: 100%;\n}\na[data-v-c7cce5be] {\r\n    color: black;\n}\r\n", ""]);
 
 // exports
 
@@ -61276,7 +61362,7 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "row" }, [
-    _c("div", { staticClass: "col-md-8" }, [_vm._v("Checkout form")]),
+    _vm._m(0),
     _vm._v(" "),
     _c(
       "div",
@@ -61286,7 +61372,7 @@ var render = function() {
           _c(
             "h6",
             { staticClass: "text-uppercase text-secondary font-weight-bolder" },
-            [_vm._v("Your cart")]
+            [_vm._v("\n                Your cart\n            ")]
           ),
           _vm._v(" "),
           _c("h6", { staticClass: "badge badge-secondary text-uppercase" }, [
@@ -61320,7 +61406,7 @@ var render = function() {
                     },
                     [
                       _vm._v(
-                        "\n                    " +
+                        "\n                        " +
                           _vm._s(item.bookable.title) +
                           "\n                    "
                       )
@@ -61370,7 +61456,119 @@ var render = function() {
     )
   ])
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "col-md-8" }, [
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-6 form-group" }, [
+          _c("label", { attrs: { for: "first_name" } }, [_vm._v("First name")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: {
+              type: "text",
+              name: "first_name",
+              id: "",
+              placeholder: "First name"
+            }
+          })
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-md-6 form-group" }, [
+          _c("label", { attrs: { for: "last_name" } }, [_vm._v("Last name")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: {
+              type: "text",
+              name: "last_name",
+              id: "",
+              placeholder: "Last name"
+            }
+          })
+        ])
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-12 form-group" }, [
+          _c("label", { attrs: { for: "email" } }, [_vm._v("Email")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "email", name: "last_name", id: "" }
+          })
+        ])
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-6 form-group" }, [
+          _c("label", { attrs: { for: "street" } }, [_vm._v("Street")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", name: "street", id: "" }
+          })
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-md-6 form-group" }, [
+          _c("label", { attrs: { for: "city" } }, [_vm._v("City")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", name: "city", id: "" }
+          })
+        ])
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-6 form-group" }, [
+          _c("label", { attrs: { for: "country" } }, [_vm._v("Country")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", name: "country", id: "" }
+          })
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-md-4 form-group" }, [
+          _c("label", { attrs: { for: "state" } }, [_vm._v("State")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", name: "state", id: "" }
+          })
+        ]),
+        _vm._v(" "),
+        _c("div", { staticClass: "col-md-2 form-group" }, [
+          _c("label", { attrs: { for: "zip" } }, [_vm._v("Zip")]),
+          _vm._v(" "),
+          _c("input", {
+            staticClass: "form-control",
+            attrs: { type: "text", name: "zip", id: "" }
+          })
+        ])
+      ]),
+      _vm._v(" "),
+      _c("hr"),
+      _vm._v(" "),
+      _c("div", { staticClass: "row" }, [
+        _c("div", { staticClass: "col-md-12 form-group" }, [
+          _c(
+            "button",
+            {
+              staticClass: "btn btn-lg btn-primary btn-block",
+              attrs: { type: "submit" }
+            },
+            [_vm._v("Book now!")]
+          )
+        ])
+      ])
+    ])
+  }
+]
 render._withStripped = true
 
 
--- resources/js/Basket/Basket.vue
+++ resources/js/Basket/Basket.vue
@@ -1,55 +1,140 @@
 <template>
     <div class="row">
-        <div class="col-md-8">Checkout form</div>
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-6 form-group">
+                    <label for="first_name">First name</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        name="first_name"
+                        id=""
+                        placeholder="First name"
+                    />
+                </div>
+                 <div class="col-md-6 form-group">
+                    <label for="last_name">Last name</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        name="last_name"
+                        id=""
+                        placeholder="Last name"
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 form-group">
+                    <label for="email">Email</label>
+                    <input
+                        type="email"
+                        class="form-control"
+                        name="last_name"
+                        id=""
+                    />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 form-group">
+                    <label for="street">Street</label>
+                    <input type="text" class="form-control" name="street" id="">
+                </div>
+                <div class="col-md-6 form-group">
+                    <label for="city">City</label>
+                    <input type="text" class="form-control" name="city" id="">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 form-group">
+                    <label for="country">Country</label>
+                    <input type="text" class="form-control" name="country" id="">
+                </div>
+                <div class="col-md-4 form-group">
+                    <label for="state">State</label>
+                    <input type="text" class="form-control" name="state" id="">
+                </div>
+                <div class="col-md-2 form-group">
+                    <label for="zip">Zip</label>
+                    <input type="text" class="form-control" name="zip" id="">
+                </div>
+            </div>
+            <hr/>
+            <div class="row">
+                <div class="col-md-12 form-group">
+                    <button type="submit" class="btn btn-lg btn-primary btn-block">Book now!</button>
+                </div>
+            </div>
+
+        </div>
         <div class="col-md-4">
             <div class="d-flex justify-content-between">
-                <h6 class="text-uppercase text-secondary font-weight-bolder">Your cart</h6>
+                <h6 class="text-uppercase text-secondary font-weight-bolder">
+                    Your cart
+                </h6>
                 <h6 class="badge badge-secondary text-uppercase">
-                    <span v-if="itemsInBasket">Items {{itemsInBasket}}</span>
+                    <span v-if="itemsInBasket">Items {{ itemsInBasket }}</span>
                     <span v-else>Empty</span>
                 </h6>
             </div>
             <transition-group name="fade">
-<div v-for="item in basket" :key="item.bookable.id">
-                <div class="pt-2 pb-2 border-top d-flex justify-content-between">
-                    <router-link :to="{name: 'bookable', params:{id:item.bookable.id}}">
-                    {{item.bookable.title}}
-                    </router-link>
-                    <span class="font-weight-bold">{{item.price.total}}</span>
-                </div>
+                <div v-for="item in basket" :key="item.bookable.id">
+                    <div
+                        class="pt-2 pb-2 border-top d-flex justify-content-between"
+                    >
+                        <router-link
+                            :to="{
+                                name: 'bookable',
+                                params: { id: item.bookable.id }
+                            }"
+                        >
+                            {{ item.bookable.title }}
+                        </router-link>
+                        <span class="font-weight-bold">{{
+                            item.price.total
+                        }}</span>
+                    </div>
 
-                <div class="pt-2 pb-2  d-flex justify-content-between">
-                    <span>From {{item.dates.from}}</span>
+                    <div class="pt-2 pb-2  d-flex justify-content-between">
+                        <span>From {{ item.dates.from }}</span>
 
-                    <span>To {{item.dates.to}}</span>
+                        <span>To {{ item.dates.to }}</span>
+                    </div>
+                    <div class="pt-2 pb-2  text-right">
+                        <button
+                            class="btn btn-sm btn-outline-secondary"
+                            @click="
+                                $store.dispatch(
+                                    'removeFromBasket',
+                                    item.bookable.id
+                                )
+                            "
+                        >
+                            <i class="fas fa-trash-alt"></i>
+                        </button>
+                    </div>
                 </div>
-                <div class="pt-2 pb-2  text-right">
-                    <button class="btn btn-sm btn-outline-secondary" @click="$store.dispatch('removeFromBasket', item.bookable.id)"><i class="fas fa-trash-alt"></i></button>
-                </div>
-            </div>
             </transition-group>
-            
         </div>
     </div>
 </template>
 <script>
-import {mapGetters, mapState} from 'vuex'
+import { mapGetters, mapState } from "vuex";
 export default {
-    computed:{
-        ...mapGetters(['itemsInBasket']),
+    computed: {
+        ...mapGetters(["itemsInBasket"]),
         ...mapState({
             basket: state => state.basket.items
         })
     }
-    
-}
+};
 </script>
 <style scoped>
-h6.badge{
+h6.badge {
     font-size: 100%;
 }
 
-a{
+a {
     color: black;
 }
 </style>
