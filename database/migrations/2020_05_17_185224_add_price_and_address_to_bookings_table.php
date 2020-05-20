56ca65c51d8ecfcf82cbe5b1c5be50e23ea08949
unknown <artykbaev.nurdaulet@mail.ru>
2020-05-21 01:28:18 +0600

    section 12: 3. Laravel Sanctum installation,4. Sanctum configuration - domains

--- database/migrations/2020_05_17_185224_add_price_and_address_to_bookings_table.php
+++ database/migrations/2020_05_17_185224_add_price_and_address_to_bookings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPriceAndAddressToBookingTable extends Migration
+class AddPriceAndAddressToBookingsTable extends Migration
 {
     /**
      * Run the migrations.
